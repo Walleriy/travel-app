f34220a7598c6c84ea5eb1c02a64ce35b65e8686
Anton Maryn <tonysmaryn@gmail.com>
2021-03-11 23:07:47 +0300

    change client structure

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.scss';
+import { MainPage } from './pages/main/MainPage';
+//import {CountryPage} from './pages/country/CountryPage'
+
+function App() {
+  return <MainPage />;
+}
+
+export default App;
