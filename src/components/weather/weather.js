d604af750af43b19915b6b1e4d16b22c7eb4d485
Walleriy <walleriystepanenko@gmail.com>
2021-03-16 21:37:52 +0200

    fix: weather using API

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import './weather.scss'
 import {useTranslation} from "react-i18next";
 
@@ -7,24 +7,28 @@ const Weather = ({city}) => {
     const {t} = useTranslation('translations');
     const [weatherData, setWeatherData] = useState('')
 
-    const getWeather = async () => {
-        const KeyWeather = 'a20a4b17a5bdf77bf7b0b7a925a9ee6d';
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KeyWeather}&lang=ru&units=metric`;
-        const result = await fetch(url);
-        const data = await result.json();
-        if (data['message'] === 'city not found') {
-            setWeatherData('Місто не знайдено!');
-        } else {
-            let res = {};
-            res.weatherIconClassName = `weather-icon owf owf-${data.weather[0].id}`;
-            res.temperature = `${t('weather.temperature')}${Math.round(data.main.temp)}°C `;
-            res.humidity = `${t('weather.humidity')}${data.main.humidity}% `;
-            res.wind = `${t('weather.wind')}${Math.round(data.wind.speed)} м/c `;
-            setWeatherData(res);
-        }
-    }
 
-    getWeather();
+
+    useEffect(() => {
+        async function getWeather() {
+            const KeyWeather = 'a20a4b17a5bdf77bf7b0b7a925a9ee6d';
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KeyWeather}&lang=ru&units=metric`;
+            const result = await fetch(url);
+            const data = await result.json();
+            if (data['message'] === 'city not found') {
+                setWeatherData('Місто не знайдено!');
+            } else {
+                let res = {};
+                res.weatherIconClassName = `weather-icon owf owf-${data.weather[0].id}`;
+                res.temperature = `${t('weather.temperature')}${Math.round(data.main.temp)}°C `;
+                res.humidity = `${t('weather.humidity')}${data.main.humidity}% `;
+                res.wind = `${t('weather.wind')}${Math.round(data.wind.speed)} м/c `;
+                setWeatherData(res);
+            }
+        }
+
+        getWeather();
+    }, [city, t]);
 
     return (
         <div className="weather">
