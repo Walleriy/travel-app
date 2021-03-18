f5851b3db1318b2465ed87b9b890159ff6e1a4ab
Anton Maryn <tonysmaryn@gmail.com>
2021-03-18 11:30:34 +0300

    fix map bug

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -4,27 +4,29 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export const Map = ({ coordinate }) => {
-  let coord = null;
+  let coord = [];
   if (coordinate && coordinate.coordinates) {
-    coord = coordinate.coordinates;
+    coord[0] = coordinate.coordinates[1];
+    coord[1] = coordinate.coordinates[0];
+    console.log(coord);
+    return (
+      <MapContainer
+        className="map"
+        center={coord}
+        zoom={5}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={coord}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
   }
-
-  return (
-    <MapContainer
-      className="map"
-      center={coord}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={coord}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+  return <></>;
 };
